6e519dffc15598dd3ca0b8fc7fd33f93ac56909e
Louis <836250617@qq.com>
2024-12-31 16:26:15 +0800

    log

--- src/event.rs
+++ src/event.rs
@@ -4,15 +4,8 @@ pub struct EventPlugin;
 
 impl Plugin for EventPlugin {
     fn build(&self, app: &mut App) {
-        app.add_event::<MouseClickEvent>();
-    }
-}
-
-pub struct DebugEventPlugin;
-
-impl Plugin for DebugEventPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(Update, debug_event);
+        app.add_event::<MouseClickEvent>()
+            .add_systems(Update, event);
     }
 }
 
@@ -23,8 +16,8 @@ pub enum MouseClickEvent {
     None,
 }
 
-fn debug_event(mut ev_r: EventReader<MouseClickEvent>) {
+fn event(mut ev_r: EventReader<MouseClickEvent>) {
     for ev in ev_r.read() {
-        println!("{:?}", ev);
+        debug!("{:?}", ev);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 mod event;
 mod input;
 mod setup;
 
-use bevy::prelude::*;
-use event::{DebugEventPlugin, EventPlugin};
+use bevy::{
+    log::{Level, LogPlugin},
+    prelude::*,
+};
+use event::EventPlugin;
 use input::MouseClickPlugin;
 use setup::setup;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(MouseClickPlugin)
-        .add_plugins(DebugEventPlugin)
+    let mut app = App::new();
+    #[cfg(debug_assertions)]
+    app.add_plugins(DefaultPlugins.set(LogPlugin {
+        level: Level::DEBUG,
+        ..Default::default()
+    }));
+    #[cfg(not(debug_assertions))]
+    app.add_plugins(DefaultPlugins.set(LogPlugin {
+        level: Level::INFO,
+        ..Default::default()
+    }));
+    app.add_plugins(MouseClickPlugin)
         .add_plugins(EventPlugin)
         .add_systems(Startup, setup)
         .run();
